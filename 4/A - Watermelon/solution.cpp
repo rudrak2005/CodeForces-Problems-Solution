f6ea0d706e677bec434950e80fa37bd8fc49afd3
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-07-05 23:56:09 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int w;
+    cin>>w;
+    if(w % 2 == 0 && w > 2){
+        cout<<"YES
+";
+    }
+    else{
+        cout<<"NO
+";
+    }
+   return 0;
+}
