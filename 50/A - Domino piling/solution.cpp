94be242a9ebab983dc5dd5f82fa675ad6dd4e6f9
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-07-10 23:53:16 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ int m,n;
+ cin>>m>>n;
+ cout<<(m*n)/2<<endl;
+    return 0;
+}
