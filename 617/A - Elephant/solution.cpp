61b79feb1eae28fb34ef9f819eb4b838092de8e0
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-07-19 11:01:10 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   int x;
+   cin>>x;
+   cout<<(x+4)/5<<"
+";
+    return 0;
+}
