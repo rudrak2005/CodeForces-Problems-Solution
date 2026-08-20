e929ec4320cb5a3cf65f4f3fd9f596a38add45e6
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-08-20 21:26:54 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+	
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+	int n;
+	cin>>n;
+	double sum =0;
+	
+	for(int i = 0; i<n; i++){
+		double x;
+		cin>>x;
+		sum += x;
+	}
+	cout<<sum/n;
+	
+		
+		
+	 return 0;
+}
