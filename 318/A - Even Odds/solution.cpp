9aadc8becdd694e033be8f4c65e766d6aeaacc3c
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-08-24 19:52:37 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+	
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+	long long n, k;
+	cin>>n>>k;
+	long long oddCount = (n+1)/2;
+	if(k <=oddCount) {
+		cout<<2*k-1<<'
+';
+	}
+	else{
+		cout<<2*(k-oddCount)<<'
+';
+	}
+		
+		
+	 return 0;
+}
