62346ea857e7c8d31b0698ed58b73411c3f6f4f2
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-08-26 21:04:17 +0530

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+	
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	string s1, s2;
+	cin>>s1>>s2;
+	for(int i =0; i<s1.size(); i++){
+		if(s1[i] == s2[i]){
+			cout<<'0';
+		}
+	
+		else{
+			cout<<'1';
+		}
+	}
+	cout<<'
+';
+		
+		
+	 return 0;
+}
